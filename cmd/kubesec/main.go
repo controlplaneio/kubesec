501a88d50983779057a1dd1a1edc067f1fa75c5d
stefanprodan <stefan.prodan@gmail.com>
2019-01-29 15:09:33 +0200

    Init kubesec go CLI - add version command - add GoReleaser config

---
+++ cmd/kubesec/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	logger *zap.Logger
+
+	// vars injected by goreleaser at build time
+	version = "unknown"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "kubesec",
+	Short: "kubesec command line",
+	Long: `
+Validate Kubernetes resource security policies`,
+}
+
+func main() {
+	var err error
+
+	// logger writes to stderr
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+	defer logger.Sync()
+
+	rootCmd.SetArgs(os.Args[1:])
+	if err := rootCmd.Execute(); err != nil {
+		e := err.Error()
+		fmt.Println(strings.ToUpper(e[:1]) + e[1:])
+		os.Exit(1)
+	}
+}
