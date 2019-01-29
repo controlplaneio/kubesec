aec374d7477c70c31f45d2d57a326c10511f8799
stefanprodan <stefan.prodan@gmail.com>
2019-01-29 22:16:37 +0200

    Add security context rules - fix host network rule

---
+++ pkg/rules/runAsUser_test.go
@@ -0,0 +1,39 @@
+package rules
+
+import (
+	"github.com/ghodss/yaml"
+	"testing"
+)
+
+func Test_RunAsUser_InitContainers(t *testing.T) {
+	var data = `
+---
+apiVersion: apps/v1
+kind: Deployment
+spec:
+  template:
+    spec:
+      initContainers:
+        - name: init1
+          securityContext:
+            runAsUser: 1
+        - name: init2
+          securityContext:
+            runAsUser: 10001
+      containers:
+        - name: c1
+        - name: c2
+          securityContext:
+            runAsUser: 999
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := RunAsUser(json)
+	if containers != 3 {
+		t.Errorf("Got %v containers wanted %v", containers, 3)
+	}
+}
