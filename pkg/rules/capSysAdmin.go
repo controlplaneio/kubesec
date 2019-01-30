01599c856f9cb681fa9d47d7beeca42c48954aff
stefanprodan <stefan.prodan@gmail.com>
2019-01-30 13:26:02 +0200

    Add CAP_SYS_ADMIN rule

---
+++ pkg/rules/capSysAdmin.go
@@ -0,0 +1,31 @@
+package rules
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/thedevsaddam/gojsonq"
+	"strings"
+)
+
+func CapSysAdmin(json []byte) int {
+	spec := getSpecSelector(json)
+	containers := 0
+
+	capAdd := gojsonq.New().Reader(bytes.NewReader(json)).
+		From(spec + ".containers").
+		Only("securityContext.capabilities.add")
+
+	if capAdd != nil && strings.Contains(fmt.Sprintf("%v", capAdd), "SYS_ADMIN") {
+		containers++
+	}
+
+	capAddInit := gojsonq.New().Reader(bytes.NewReader(json)).
+		From(spec + ".initContainers").
+		Only("securityContext.capabilities.add")
+
+	if capAddInit != nil && strings.Contains(fmt.Sprintf("%v", capAddInit), "SYS_ADMIN") {
+		containers++
+	}
+
+	return containers
+}
