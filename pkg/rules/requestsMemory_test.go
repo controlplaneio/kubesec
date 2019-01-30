7aaeeaa9dba12b037aba8158d985e199f653373b
stefanprodan <stefan.prodan@gmail.com>
2019-01-30 20:27:53 +0200

    Add memory limits and requests rules

---
+++ pkg/rules/requestsMemory_test.go
@@ -0,0 +1,37 @@
+package rules
+
+import (
+	"github.com/ghodss/yaml"
+	"testing"
+)
+
+func Test_RequestsMemory_Pod(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: c1
+    resources:
+      limits:
+       memory: 200Mi
+      requests:
+       memory: 100Mi
+  - name: c2
+    resources:
+      limits:
+       cpu: 100m
+  - name: c3
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := RequestsMemory(json)
+	if containers != 1 {
+		t.Errorf("Got %v containers wanted %v", containers, 1)
+	}
+}
