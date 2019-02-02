cad1a3825993703109ecf7385af0404106fe2416
Andrew Martin <sublimino@gmail.com>
2019-02-02 17:47:59 +0100

    feat: add capDropAll and capDropAny

---
+++ pkg/rules/capDropAll_test.go
@@ -0,0 +1,117 @@
+package rules
+
+import (
+	"github.com/ghodss/yaml"
+	"testing"
+)
+
+func Test_CapDropAll_Pod(t *testing.T) {
+  var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: c1
+    securityContext:
+      capabilities:
+        drop:
+          - ALL
+  - name: c2
+    securityContext:
+      capabilities:
+  - name: c3
+`
+
+  json, err := yaml.YAMLToJSON([]byte(data))
+  if err != nil {
+    t.Fatal(err.Error())
+  }
+
+  containers := CapDropAll(json)
+  if containers != 1 {
+    t.Errorf("Got %v containers wanted %v", containers, 1)
+  }
+}
+
+func Test_CapDropAllMissing_Pod(t *testing.T) {
+  var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: c1
+    securityContext:
+      capabilities:
+        drop: 
+  - name: c2
+    securityContext:
+      capabilities:
+  - name: c3
+`
+
+  json, err := yaml.YAMLToJSON([]byte(data))
+  if err != nil {
+    t.Fatal(err.Error())
+  }
+
+  containers := CapDropAll(json)
+  if containers != 0 {
+    t.Errorf("Got %v containers wanted %v", containers, 0)
+  }
+}
+
+func Test_CapDropAll_InitContainers(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+  initContainers:
+  - name: init1
+    securityContext:
+      capabilities:
+        drop:
+          - ALL
+  containers:
+  - name: c1
+    securityContext:
+      capabilities:
+        drop:
+          - ALL
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := CapDropAll(json)
+	if containers != 2 {
+		t.Errorf("Got %v containers wanted %v", containers, 2)
+	}
+}
+
+func Test_CapDropAll_Missing(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+  initContainers:
+  - name: init1
+  containers:
+  - name: c1
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := CapDropAll(json)
+	if containers != 0 {
+		t.Errorf("Got %v containers wanted %v", containers, 0)
+	}
+}
