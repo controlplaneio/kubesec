a3d25769e402b7fb832351cc71286dd53b5ba073
Andrew Martin <sublimino@gmail.com>
2019-02-17 16:08:39 +0000

    refactor: update rules to return number of matches

--- pkg/rules/requestsCPU_test.go
+++ pkg/rules/requestsCPU_test.go
@@ -14,14 +14,74 @@ spec:
   containers:
   - name: c1
     resources:
-      limits:
-       cpu: 300m
       requests:
-       cpu: 300m
+        cpu: 300m
   - name: c2
     resources:
       requests:
   - name: c3
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := RequestsCPU(json)
+	if containers != 1 {
+		t.Errorf("Got %v containers wanted %v", containers, 1)
+	}
+
+	//t.Errorf("FAKE ERROR")
+
+}
+
+// TODO(ajm) will this be validated by kubeval instead?
+//func Test_RequestsCPU_Empty_Value_Pod(t *testing.T) {
+//  var data = `
+//---
+//apiVersion: v1
+//kind: Pod
+//spec:
+//  containers:
+//  - name: c1
+//    resources:
+//      requests:
+//        cpu:
+//  - name: c2
+//    resources:
+//      requests:
+//  - name: c3
+//`
+//
+//  json, err := yaml.YAMLToJSON([]byte(data))
+//  if err != nil {
+//    t.Fatal(err.Error())
+//  }
+//
+//  containers := RequestsCPU(json)
+//  if containers != 0 {
+//    t.Errorf("Got %v containers wanted %v", containers, 0)
+//  }
+//}
+
+func Test_RequestsCPU_With_Limits_Pod(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+ containers:
+ - name: c1
+   resources:
+     limits:
+       cpu: 300m
+     requests:
+       cpu: 300m
+ - name: c2
+   resources:
+     requests:
+ - name: c3
 `
 
 	json, err := yaml.YAMLToJSON([]byte(data))
