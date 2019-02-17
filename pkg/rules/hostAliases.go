da2a3e910eadeb656ad8a79e1f19d796f0b8ae80
Andrew Martin <sublimino@gmail.com>
2019-02-17 17:00:29 +0000

    feat: add hostAliases

---
+++ pkg/rules/hostAliases.go
@@ -0,0 +1,21 @@
+package rules
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/thedevsaddam/gojsonq"
+)
+
+func HostAliases(json []byte) int {
+	spec := getSpecSelector(json)
+
+	jqContainers := gojsonq.New().Reader(bytes.NewReader(json)).
+		From(spec+".hostAliases")
+
+	// TODO(ajm) the above `Where` selectors don't do what I'd expect and filter the results
+	if fmt.Sprintf("%v", jqContainers.Get()) != "<nil>" {
+		return 1
+	}
+
+	return 0
+}
