276964b96fe7bdd2c1b36237f5d4707d55303962
Andrew Martin <sublimino@gmail.com>
2019-02-28 18:46:35 +0100

    feat: add seccomp any rule
    - rule passes if there is any seccomp rule that isn't unconfined

--- pkg/ruler/ruleset.go
+++ pkg/ruler/ruleset.go
@@ -154,6 +154,15 @@ func NewRuleset(logger *zap.SugaredLogger) *Ruleset {
 	}
 	list = append(list, limitsMemoryRule)
 
+	seccompAnyRule := Rule{
+		Predicate: rules.SeccompAny,
+		Selector:  ".metadata .annotations .\"container.seccomp.security.alpha.kubernetes.io/pod\"",
+		Reason:    "Seccomp profiles set minimum privilege and secure against unknown threats",
+		Kinds:     []string{"Pod", "Deployment", "StatefulSet", "DaemonSet"},
+		Points:    1,
+	}
+	list = append(list, seccompAnyRule)
+
 	return &Ruleset{
 		Rules:  list,
 		logger: logger,
