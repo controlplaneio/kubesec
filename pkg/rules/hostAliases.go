00f45184b7042d6c2751c8817cf00286d0504717
Andrew Martin <sublimino@gmail.com>
2019-03-02 13:14:08 +0000

    fix: gofmt

--- pkg/rules/hostAliases.go
+++ pkg/rules/hostAliases.go
@@ -10,7 +10,7 @@ func HostAliases(json []byte) int {
 	spec := getSpecSelector(json)
 
 	jqContainers := gojsonq.New().Reader(bytes.NewReader(json)).
-		From(spec+".hostAliases")
+		From(spec + ".hostAliases")
 
 	// TODO(ajm) the above `Where` selectors don't do what I'd expect and filter the results
 	if fmt.Sprintf("%v", jqContainers.Get()) != "<nil>" {
