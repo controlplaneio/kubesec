647237c51e80093550f2a34a86b4a4484b50760a
Andrew Martin <sublimino@gmail.com>
2019-03-03 19:31:28 +0000

    feat: add seccomp any for named pod

--- pkg/rules/seccompAny.go
+++ pkg/rules/seccompAny.go
@@ -4,21 +4,37 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/thedevsaddam/gojsonq"
+	"regexp"
 	"strings"
 )
 
-// TODO(ajm) this should be checking for the name of the pod, not `pod`, so needs wildcard matching
+// TODO(ajm): tighten these matches, they could be "[seccomp..." or " seccomp...", and "unconfined]" or "unconfined "
+// TODO(ajm): space delimiting matches is insufficient as this could be set to `unconfined blah`
 func SeccompAny(json []byte) int {
 	containers := 0
+	startWordBoundaryRegex := "[\\[ ]"
+	endWordBoundaryRegex := "[\\] ]"
 
 	capDrop := gojsonq.New().Reader(bytes.NewReader(json)).
 		From("metadata.annotations").Get()
 
-	if capDrop != nil && strings.Contains(fmt.Sprintf("%v", capDrop), "seccomp.security.alpha.kubernetes.io/pod:") {
-		// TODO(ajm): tighten these matches, they could be "[seccomp..." or " seccomp...", and "unconfined]" or "unconfined "
-		// TODO(ajm): space delimiting matches is insufficient as this could be set to `unconfined blah`
-		if !strings.Contains(fmt.Sprintf("%v", capDrop), "seccomp.security.alpha.kubernetes.io/pod:unconfined") {
+	capDropString := fmt.Sprintf("%v", capDrop)
+
+	if capDrop != nil && strings.Contains(capDropString, "seccomp.security.alpha.kubernetes.io/pod:") {
+		if !strings.Contains(capDropString, "seccomp.security.alpha.kubernetes.io/pod:unconfined") {
 			containers++
+		}
+	} else if capDrop != nil {
+
+		keyNameRegex := "seccomp\\.security\\.alpha\\.kubernetes\\.io/[a-zA-Z-.]+"
+		// TODO(ajm) match end of string in regex
+		isNamedPodMatch, _ := regexp.MatchString(startWordBoundaryRegex+keyNameRegex+":", capDropString)
+
+		if isNamedPodMatch == true {
+			isUnconfinedNamedPodMatch, _ := regexp.MatchString(startWordBoundaryRegex+keyNameRegex+":unconfined"+endWordBoundaryRegex, capDropString)
+			if !isUnconfinedNamedPodMatch {
+				containers++
+			}
 		}
 	}
 
