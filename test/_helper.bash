76829fa4cf5262699fa928d08157f0007f74bd0f
Andrew Martin <sublimino@gmail.com>
2019-04-10 00:08:53 +0100

    feat: most acceptance tests passing
    - some large scoring changes
      - the previous version scored once per deployment
      - this version scores per-container

--- test/_helper.bash
+++ test/_helper.bash
@@ -5,24 +5,39 @@ load './bin/bats-assert/load'
 
 TEST_DIR="."
 
+_global_setup() {
+    [ ! -f ${BATS_PARENT_TMPNAME}.skip ] || skip "skip remaining tests"
+}
+
+_global_teardown() {
+    if [ ! -n "$BATS_TEST_COMPLETED" ]; then
+      touch ${BATS_PARENT_TMPNAME}.skip
+    fi
+}
+
 _is_local() {
   [[ "${TEST_REMOTE:-0}" != 1 ]]
 }
+
 _is_remote() {
   ! _is_local
 }
+
 _get_remote_url() {
   echo "${REMOTE_URL:-https://kubesec.io/}"
 }
+
 if _is_remote; then
 
   _app() {
     local FILE="${1:-}"
     shift
-    curl --fail -v "$(_get_remote_url)" -F file=@"${FILE}" "${@}"
+    local ARGS="${@:-}"
+    ARGS=$(echo "${ARGS}" | sed 's,--json,,g')
+    curl --fail -v "$(_get_remote_url)" -F file=@"${FILE}" "${ARGS}"
   }
 
-  assert_non_zero_points() {
+  assert_gt_zero_points() {
     assert_output --regexp ".*\"score\": [1-9]+,.*"
     assert_success
   }
@@ -38,16 +53,30 @@ if _is_remote; then
   }
 
   assert_file_not_found() {
-    assert_output --regexp ".*couldn't open file \"somefile.yaml\".*"
+    assert_output --regexp ".*couldn't open file \"somefile.yaml\".*" \
+     || assert_output --regexp ".*somefile.yaml: no such file or directory.*"
+  }
+
+  assert_invalid_input() {
+    assert_output --regexp '  "error": "Invalid input"' \
+     || assert_output --regexp ".*Document is invalid, no Kubernetes kind found.*"
   }
 
   assert_failure_local() { :; }
 
 else
 
-  _app() { ./../${BIN_UNDER_TEST:-kseccheck.sh} "${@}"; }
+  _app() {
+    local ARGS="${@:-}"
+    if [[ "${BIN_UNDER_TEST}" != "" ]]; then
+      # remove --json flag for golang rewrite
+      ARGS=$(echo "${ARGS}" | sed -E 's,--json,,g')
+    fi
+    echo "# DEBUG: ARGS ${ARGS}" >&3
+    ./../${BIN_UNDER_TEST:-kseccheck.sh} "${ARGS}";
+  }
 
-  assert_non_zero_points() {
+  assert_gt_zero_points() {
     assert_output --regexp ".*with a score of [1-9]+ points.*"
     assert_success
   }
@@ -63,7 +92,15 @@ else
   }
 
   assert_file_not_found() {
-    assert_output --regexp ".*File somefile.yaml does not exist.*"
+    assert_output --regexp ".*File somefile.yaml does not exist.*" \
+     || assert_output --regexp ".*somefile.yaml: no such file or directory.*"  \
+     || assert_output --regexp ".*no such file or directory.*"
+  }
+
+  assert_invalid_input() {
+    assert_output --regexp '  "error": "Invalid input"' \
+     || assert_output --regexp ".*Document is invalid, no Kubernetes kind found.*" \
+     || assert_output --regexp ".*no such file or directory.*"
   }
 
   assert_failure_local() {
