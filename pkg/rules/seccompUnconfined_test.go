4325f91307340f36cd3ff45546e0d4fdeae928a8
Andrew Martin <sublimino@gmail.com>
2019-04-09 23:54:08 +0100

    feat: seccompUnconfined

---
+++ pkg/rules/seccompUnconfined_test.go
@@ -0,0 +1,189 @@
+package rules
+
+import (
+	"github.com/ghodss/yaml"
+	"testing"
+)
+
+func Test_SeccompUnconfined_Pod(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    other: runtime/default
+    seccomp.security.alpha.kubernetes.io/pod: runtime/default
+    something: runtime/default
+spec:
+  containers:
+    - name: trustworthy-container
+      image: sotrustworthy:latest
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 0 {
+		t.Errorf("Got %v containers wanted %v", containers, 0)
+	}
+}
+
+func Test_SeccompUnconfined_Pod_Unconfined(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    seccomp.security.alpha.kubernetes.io/pod: unconfined
+spec:
+  containers:
+    - name: trustworthy-container
+      image: sotrustworthy:latest
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 1 {
+		t.Errorf("Got %v containers wanted %v", containers, 1)
+	}
+}
+
+func Test_SeccompUnconfined_No_Seccomp(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+spec:
+  containers:
+  - name: c1
+    securityContext:
+      capabilities:
+        drop:
+  - name: c2
+    securityContext:
+      capabilities:
+  - name: c3
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 0 {
+		t.Errorf("Got %v containers wanted %v", containers, 0)
+	}
+}
+
+func Test_SeccompUnconfined_Named_Pod(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    seccomp.security.alpha.kubernetes.io/somePodName: runtime/default
+spec:
+  containers:
+    - name: somePodName
+      image: sotrustworthy:latest
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 0 {
+		t.Errorf("Got %v containers wanted %v", containers, 0)
+	}
+}
+
+func Test_SeccompUnconfined_Named_Pod_Special_Chars(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    seccomp.security.alpha.kubernetes.io/my-Named.Pod: runtime/default
+spec:
+  containers:
+    - name: trustworthy-container
+      image: sotrustworthy:latest
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 0 {
+		t.Errorf("Got %v containers wanted %v", containers, 0)
+	}
+}
+
+// TODO(ajm) this matches the `unconfined`, but the pod name is invalid. Could lead to unexpected results on CLI (API server should only be sending valid pods for Admission Control)
+func Test_SeccompUnconfined_Named_Pod_Special_Chars_Unconfined(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+  annotations:
+    seccomp.security.alpha.kubernetes.io/my-Named.Pod: unconfined
+spec:
+  containers:
+    - name: trustworthy-container
+      image: sotrustworthy:latest
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 1 {
+		t.Errorf("Got %v containers wanted %v", containers, 1)
+	}
+}
+
+func Test_SeccompUnconfined_Named_Pod_Illegal_Name(t *testing.T) {
+	var data = `
+---
+apiVersion: v1
+kind: Pod
+metadata:
+ annotations:
+   seccomp.security.alpha.kubernetes.io/my-Named.Pod(illegal name): runtime/default
+spec:
+ containers:
+   - name: trustworthy-container
+     image: sotrustworthy:latest
+`
+
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := SeccompUnconfined(json)
+	if containers != 0 {
+		t.Errorf("Got %v containers wanted %v", containers, 0)
+	}
+}
+
+// TODO(ajm) more seccomp tests for deployments
