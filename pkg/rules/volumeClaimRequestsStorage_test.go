397a4bd9ceb0a3dab02f31fa7fd0b7dbebbac2f8
Andrew Martin <sublimino@gmail.com>
2019-04-19 14:25:21 +0100

    feat: VolumeClaimRequestsStorage

---
+++ pkg/rules/volumeClaimRequestsStorage_test.go
@@ -0,0 +1,81 @@
+package rules
+
+import (
+	"github.com/ghodss/yaml"
+	"testing"
+)
+
+func Test_VolumeClaimVolumeClaimRequestsStorage(t *testing.T) {
+	var data = `
+---
+apiVersion: "apps/v1"
+kind: StatefulSet
+spec:
+  template:
+    spec:
+      containers:
+      - name: cassandra
+        image: gcr.io/google-samples/cassandra:v14
+  volumeClaimTemplates:
+  - metadata:
+      name: www
+    spec:
+      accessModes: [ "ReadWrite" ]
+  - metadata:
+      name: 2nd
+    spec:
+      accessModes: [ "ReadWrite" ]
+      resources:
+        requests:
+          storage: 1Gi
+
+`
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := VolumeClaimRequestsStorage(json)
+	if containers != 1 {
+		t.Errorf("Got %v containers wanted %v", containers, 1)
+	}
+}
+
+func Test_VolumeClaimVolumeClaimRequestsStorage_Two_Claims(t *testing.T) {
+	var data = `
+---
+apiVersion: "apps/v1"
+kind: StatefulSet
+spec:
+  template:
+    spec:
+      containers:
+      - name: cassandra
+        image: gcr.io/google-samples/cassandra:v14
+  volumeClaimTemplates:
+  - metadata:
+      name: www
+    spec:
+      accessModes: [ "ReadWrite" ]
+      resources:
+        requests:
+          storage: 2Gi
+  - metadata:
+      name: 2nd
+    spec:
+      accessModes: [ "ReadWrite" ]
+      resources:
+        requests:
+          storage: 1Gi
+
+`
+	json, err := yaml.YAMLToJSON([]byte(data))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	containers := VolumeClaimRequestsStorage(json)
+	if containers != 2 {
+		t.Errorf("Got %v containers wanted %v", containers, 2)
+	}
+}
