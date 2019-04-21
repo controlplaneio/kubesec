85025b854ac671a951e5ef3e4b315fed5ce96524
stefanprodan <stefan.prodan@gmail.com>
2019-04-21 18:36:32 +0300

    Add multi-doc yaml support and debug flag

--- cmd/kubesec/http.go
+++ cmd/kubesec/http.go
@@ -4,9 +4,7 @@ import (
 	"fmt"
 	"github.com/spf13/cobra"
 	"github.com/sublimino/kubesec/pkg/server"
-  "go.uber.org/zap"
-  "go.uber.org/zap/zapcore"
-  "os"
+	"os"
 	"strconv"
 	"time"
 )
@@ -33,61 +31,9 @@ var httpCmd = &cobra.Command{
 		}
 
 		stopCh := server.SetupSignalHandler()
-		jsonLogger, _ := NewJsonLogger("info")
+		jsonLogger, _ := NewLogger("info", "json")
 
 		server.ListenAndServe(port, time.Minute, jsonLogger, stopCh)
 		return nil
 	},
-}
-
-// NewJsonLogger returns a zap sugared logger configured with json format recognized by Stackdriver
-func NewJsonLogger(logLevel string) (*zap.SugaredLogger, error) {
-  level := zap.NewAtomicLevelAt(zapcore.InfoLevel)
-  switch logLevel {
-  case "debug":
-    level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
-  case "info":
-    level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
-  case "warn":
-    level = zap.NewAtomicLevelAt(zapcore.WarnLevel)
-  case "error":
-    level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
-  case "fatal":
-    level = zap.NewAtomicLevelAt(zapcore.FatalLevel)
-  case "panic":
-    level = zap.NewAtomicLevelAt(zapcore.PanicLevel)
-  }
-
-  zapEncoderConfig := zapcore.EncoderConfig{
-    TimeKey:        "timestamp",
-    LevelKey:       "severity",
-    NameKey:        "logger",
-    CallerKey:      "caller",
-    MessageKey:     "message",
-    StacktraceKey:  "stacktrace",
-    LineEnding:     zapcore.DefaultLineEnding,
-    EncodeLevel:    zapcore.LowercaseLevelEncoder,
-    EncodeTime:     zapcore.ISO8601TimeEncoder,
-    EncodeDuration: zapcore.SecondsDurationEncoder,
-    EncodeCaller:   zapcore.ShortCallerEncoder,
-  }
-
-  zapConfig := zap.Config{
-    Level:       level,
-    Development: false,
-    Sampling: &zap.SamplingConfig{
-      Initial:    100,
-      Thereafter: 100,
-    },
-    Encoding:         "json",
-    EncoderConfig:    zapEncoderConfig,
-    OutputPaths:      []string{"stderr"},
-    ErrorOutputPaths: []string{"stderr"},
-  }
-
-  logger, err := zapConfig.Build()
-  if err != nil {
-    return nil, err
-  }
-  return logger.Sugar(), nil
 }
