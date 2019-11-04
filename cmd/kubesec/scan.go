08674662616ff5c193e55b0811d50020e1436a03
Jon Knox <me@jonknox.com>
2019-11-04 14:08:40 +0000

    feat: report sorted so identical over runs
    adds custom ordering functionality to RuleRef
    closes #44

--- cmd/kubesec/scan.go
+++ cmd/kubesec/scan.go
@@ -10,6 +10,7 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"sort"
 )
 
 type ScanFailedValidationError struct {
@@ -20,6 +21,7 @@ func (e *ScanFailedValidationError) Error() string {
 }
 
 var debug bool
+
 
 func init() {
 	scanCmd.Flags().BoolVar(&debug, "debug", false, "turn on debug logs")
@@ -70,6 +72,18 @@ var scanCmd = &cobra.Command{
 			if r.Score <= 0 {
 				lowScore = true
 				break
+			}
+		}
+
+		// Sort reports into custom order
+		for _, r := range reports {
+			if (r.Valid) {
+				if ( len(r.Scoring.Critical) >1 ) {
+					sort.Sort(ruler.RuleRefCustomOrder(r.Scoring.Critical))
+				}
+				if ( len(r.Scoring.Advise) >1 ) {
+					sort.Sort(ruler.RuleRefCustomOrder(r.Scoring.Advise))
+				}
 			}
 		}
 
