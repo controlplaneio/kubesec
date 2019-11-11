217a689dfc70bd3498a775dc011ba8ca312eef33
stefanprodan <stefan.prodan@gmail.com>
2019-11-11 21:06:32 +0200

    Fix cap drop test for gojsonq v2.3.0
    Signed-off-by: stefanprodan <stefan.prodan@gmail.com>

--- pkg/rules/capDropAny.go
+++ pkg/rules/capDropAny.go
@@ -13,23 +13,21 @@ func CapDropAny(json []byte) int {
 
 	capDrop := gojsonq.New().Reader(bytes.NewReader(json)).
 		From(spec + ".containers").
-		Select("securityContext.capabilities.drop").
-		WhereNotNil("securityContext.capabilities.drop")
+		Only("securityContext.capabilities.drop")
 
 	if capDrop != nil &&
-		capDrop.Count() > 0 &&
-		!strings.Contains(fmt.Sprintf("%v", capDrop.Get()), "<nil>") {
+		len(capDrop.([]interface{})) > 0 &&
+		!strings.Contains(fmt.Sprintf("%v", capDrop), "<nil>") {
 		containers++
 	}
 
 	capDropInit := gojsonq.New().Reader(bytes.NewReader(json)).
 		From(spec + ".initContainers").
-		Select("securityContext.capabilities.drop").
-		WhereNotNil("securityContext.capabilities.drop")
+		Only("securityContext.capabilities.drop")
 
 	if capDropInit != nil &&
-		capDropInit.Count() > 0 &&
-		!strings.Contains(fmt.Sprintf("%v", capDropInit.Get()), "<nil>") {
+		len(capDropInit.([]interface{})) > 0 &&
+		!strings.Contains(fmt.Sprintf("%v", capDropInit), "<nil>") {
 		containers++
 	}
 
